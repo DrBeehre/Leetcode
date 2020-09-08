fe9a477e3d3ff3973294e263bee3215510778117
Ward <Wardbeehre@gmail.com>
2020-09-08 18:26:20 +1200

    Moving to hash map to speed up - failing test

--- September 2020 Daily Challenges/7th - Word Pattern/Solution.java
+++ September 2020 Daily Challenges/7th - Word Pattern/Solution.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
 
 class Solution {
     public static boolean wordPattern(String pattern, String str) {
@@ -12,38 +10,28 @@ class Solution {
         // will match the pattern, so we can just simply return false;
         if(words.length != pattern.length()) return false;
 
-        List<Pair> matchingPatternStrings = new ArrayList<>();
+        HashMap<Character, String> matchingHash = new HashMap<>();
 
         for (int i = 0; i < words.length; i++) {
 
             final char patternChar = pattern.charAt(i);
             final String patternStr = words[i];
 
-            // Firstly, we need to check if the letter in the pattern has been seen before
-            Optional<Pair> matchingPairOnChar = matchingPatternStrings.stream()
-                    .filter(pair -> pair.getPatternChar() == patternChar)
-                    .findFirst();
 
-            if(matchingPairOnChar.isPresent()){
+            if(matchingHash.containsKey(patternChar)){
                 // if here,
                 // we have a matching pair.
 
                 // check matching str equals the current word,
                 // else return false.
-                if(!matchingPairOnChar.get().getMatchingString().equals(words[i])){
+                if(matchingHash.get(patternChar).equals(patternStr)){
                     return false;
                 }
             }else{
-                Optional<Pair> matchingPairOnWord = matchingPatternStrings.stream()
-                        .filter(pair -> pair.getMatchingString().equals(patternStr))
-                        .findFirst();
-
-                if(matchingPairOnWord.isPresent()){
+                if(matchingHash.containsValue(patternStr)){
                     return false;
                 }else{
-
-                    Pair newPair = new Pair(patternChar, patternStr);
-                    matchingPatternStrings.add(newPair);
+                    matchingHash.put(patternChar, patternStr);
                 }
             }
         }
