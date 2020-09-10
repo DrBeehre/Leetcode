819ba1ae20ea34310c5d2426d9c82f1c875a6ee8
WardB <wardbeehre@gmail.com>
2020-09-10 21:30:16 +1200

    Add setup for 10th day challenge 'bulls and cows'

---
+++ September 2020 Daily Challenges/10th - Bulls and Cows/BullsAndCowsSolution.java
@@ -0,0 +1,8 @@
+public class BullsAndCowsSolution {
+
+    public static String getHint(String secret, String guess) {
+
+        // TODO: needs to be replaced with an actual return value
+        return null;
+    }
+}
