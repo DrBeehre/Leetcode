0ab69edc36de3842d1df79b2f279625ce61e248e
Ward <Wardbeehre@gmail.com>
2020-09-10 13:18:54 +1200

    Add tests and inital attempt at CompareVersionSolution - Passing test but failing submission

--- September 2020 Daily Challenges/9th - Compare Version Numbers/CompareVersionSolution.java
+++ September 2020 Daily Challenges/9th - Compare Version Numbers/CompareVersionSolution.java
@@ -1,5 +1,28 @@
 public class CompareVersionSolution {
-    public int compareVersion(String version1, String version2) {
+    public static int compareVersion(String version1, String version2) {
+
+        // Split version strings up into there parts
+        String[] versionOneBreakDown = version1.split("\\.");
+        String[] versionTwoBreakDown = version2.split("\\.");
+
+        int shortestLength;
+        if(versionOneBreakDown.length > versionTwoBreakDown.length){
+            shortestLength = version2.length();
+        } else {
+            shortestLength = version1.length();
+        }
+
+        for (int i = 0; i < shortestLength; i++) {
+            Integer v1 = Integer.parseInt(versionOneBreakDown[i]);
+            Integer v2 = Integer.parseInt(versionTwoBreakDown[i]);
+
+            if(v1 > v2){
+                return 1;
+            }else if (v1 < v2){
+                return -1;
+            }
+        }
+
         return 0;
     }
 }
--- September 2020 Daily Challenges/9th - Compare Version Numbers/CompareVersionSolutionTest.java
+++ September 2020 Daily Challenges/9th - Compare Version Numbers/CompareVersionSolutionTest.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CompareVersionSolutionTest {
+
+    @Test
+    public void inital_test(){
+        String version1 = "0.1";
+        String version2 = "1.1";
+
+        assertEquals(-1, CompareVersionSolution.compareVersion(version1, version2));
+    }
+
+    @Test
+    public void inital_test_2(){
+        String version1 = "1";
+        String version2 = "01";
+
+        assertEquals(0, CompareVersionSolution.compareVersion(version1, version2));
+    }
+
 }
